483483fc9afef94e34fc12841a5b2199a3659ccc
Jackson Wearn <jacksonwearn@gmail.com>
2017-01-27 10:04:48 -0500

    Basic script to have headless browser hit amazon pro controller page

---
+++ watch.rb
@@ -0,0 +1,12 @@
+require 'watir'
+
+browser = Watir::Browser.new
+browser.goto 'https://www.amazon.com/dp/B01NAWKYZ0'
+
+if /Currently unavailable/.match(browser.text)
+  puts "Still unavailable"
+else
+  puts "It's finally here"
+end
+
+browser.close
