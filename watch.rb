ced95a6f6389d8f9db08225003b015e7bdf5d6f6
Jackson Wearn <jacksonwearn@gmail.com>
2017-01-27 10:54:30 -0500

    Add in headless gem to speed up process

--- watch.rb
+++ watch.rb
@@ -1,12 +1,16 @@
+require 'rubygems'
+require 'headless'
 require 'watir'
 
-browser = Watir::Browser.new
-browser.goto 'https://www.amazon.com/dp/B01NAWKYZ0'
+Headless.ly do
+  browser = Watir::Browser.new
+  browser.goto 'https://www.amazon.com/dp/B01NAWKYZ0'
 
-if /Currently unavailable/.match(browser.text)
-  puts "Still unavailable"
-else
-  puts "It's finally here"
+  if /Currently unavailable/.match(browser.text)
+    puts "Still unavailable"
+  else
+    puts "It's finally here"
+  end
+
+  browser.close
 end
-
-browser.close
